dbdd494eb0fd4c51cc68f5ac36e5c940f7c7e22f
Sammy <sammylintw@gmail.com>
2012-06-24 08:14:32 +0800

    first commit

---
+++ lib/redactor-rails.rb
@@ -0,0 +1,7 @@
+require "redactor-rails/version"
+
+module Redactor
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/redactor-rails/version.rb
@@ -0,0 +1,5 @@
+module Redactor
+  module Rails
+    VERSION = "0.0.1"
+  end
+end
