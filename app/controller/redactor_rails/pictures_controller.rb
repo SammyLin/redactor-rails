902e47fe54e1f9a43309a1e207a3654ce9460913
Fatih 'Computeus' AYTAÇ <fatih.aytac@gmail.com>
2012-07-18 15:17:34 +0300

    If user logged in, then photo saved.
    User can see only his uploaded photos.

--- app/controller/redactor_rails/pictures_controller.rb
+++ app/controller/redactor_rails/pictures_controller.rb
@@ -1,10 +1,9 @@
 class RedactorRails::PicturesController < RedactorRails::ApplicationController
   def index
     authorize! :access, :redactor_rails
+    @pictures = ""
     if current_user
-      @pictures = RedactorRails.picture_model.where(["assetable_id = ? AND assetable_type = 'User'", current_user.id])
-    else
-      @pictures = RedactorRails.picture_model.find_all
+      @pictures = RedactorRails.picture_model.find_all({ "assetable_id" => current_user.id, "assetable_type" => "User" })
     end
     render :json => @pictures.to_json
   end
@@ -13,12 +12,14 @@ class RedactorRails::PicturesController < RedactorRails::ApplicationController
     authorize! :access, :redactor_rails
     @picture = RedactorRails::Picture.new
 
-    file = params[:file]
-    @picture.data = RedactorRails::Http.normalize_param(file, request)
-
     if current_user
       @picture.assetable = current_user
+    else
+      render :nothing => true and return
     end
+
+    file = params[:file]
+    @picture.data = RedactorRails::Http.normalize_param(file, request)
 
     if @picture.save
       render :text => { :filelink => @picture.url(:content) }.to_json
