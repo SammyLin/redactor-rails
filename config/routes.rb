17b1fa585f8fdc3a10ca014f764006f2161a09b4
Joe Sak <joe@joesak.com>
2012-10-23 11:19:27 -0500

    Add RedactorAsset Document with DocumentUploader / DocumentsController
    Adds support for file uploads of types:
    doc, docx, pdf, xls, xlsx, txt, rtf
    
    Could add support for other types as needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 RedactorRails::Engine.routes.draw do
-    resources :pictures, :only => [:index, :create]
+  resources :pictures,  :only => [:index, :create]
+  resources :documents, :only => [:index, :create]
 end
---
+++ lib/generators/redactor/templates/base/carrierwave/uploaders/redactor_rails_document_uploader.rb
@@ -0,0 +1,14 @@
+# encoding: utf-8
+class RedactorRailsDocumentUploader < CarrierWave::Uploader::Base
+  include RedactorRails::Backend::CarrierWave
+
+  storage :fog
+
+  def store_dir
+    "system/redactor_assets/documents/#{model.id}"
+  end
+
+  def extension_white_list
+    RedactorRails.document_file_types
+  end
+end
