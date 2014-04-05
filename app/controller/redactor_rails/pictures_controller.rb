4b7af42bdba3e04df335808fe20cc8276d77f619
Debanjan Sengupta <dave4u90@gmail.com>
2014-04-05 22:06:32 +0530

    Before Filter Avoided

--- app/controller/redactor_rails/pictures_controller.rb
+++ app/controller/redactor_rails/pictures_controller.rb
@@ -1,5 +1,5 @@
 class RedactorRails::PicturesController < ApplicationController
-  before_filter :redactor_authenticate_user!
+  #before_filter :redactor_authenticate_user!
 
   def index
     @pictures = RedactorRails.picture_model.where(
