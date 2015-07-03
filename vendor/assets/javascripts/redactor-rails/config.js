67dff70db5af27590f828a1a45eab60387159961
Michael Gaganov <gaganov@gmail.com>
2015-07-03 14:51:40 +0300

    changed config.js according to new settings

--- vendor/assets/javascripts/redactor-rails/config.js
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -1,12 +1,13 @@
 window.init_redactor = function(){
   var csrf_token = $('meta[name=csrf-token]').attr('content');
   var csrf_param = $('meta[name=csrf-param]').attr('content');
-  var params;
+  var params = {};
   if (csrf_param !== undefined && csrf_token !== undefined) {
-    params = csrf_param + "=" + encodeURIComponent(csrf_token);
+      params[csrf_param] = csrf_token;
   }
   $('.redactor').redactor(
-    { "imageUpload":"/redactor_rails/pictures?" + params,
+    { "imageUpload":"/redactor_rails/pictures",
+      "uploadImageFields": params,
       "imageGetJson":"/redactor_rails/pictures",
       "path":"/assets/redactor-rails",
       "css":"style.css"}
