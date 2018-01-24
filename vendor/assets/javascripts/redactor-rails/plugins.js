3cbae2a5b872bd979872ae4fdd1e65980cc7c978
MATTHEW CLARK <matt@notch8.com>
2018-01-23 17:26:47 -0800

    updates clips require

--- vendor/assets/javascripts/redactor-rails/plugins.js
+++ vendor/assets/javascripts/redactor-rails/plugins.js
@@ -1,4 +1,4 @@
-//= require redactor-rails/plugins/clips
+//= require redactor-rails/plugins/clips/clips
 //= require redactor-rails/plugins/fontcolor
 //= require redactor-rails/plugins/fontfamily
 //= require redactor-rails/plugins/fontsize
