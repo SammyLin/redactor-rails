9dbd29dc53eaa14a2c074ab693f6394105085bbd
MATTHEW CLARK <matt@notch8.com>
2018-01-23 17:20:07 -0800

    adds config

---
+++ vendor/assets/javascripts/redactor-rails/config.js
@@ -0,0 +1,18 @@
+window.init_redactor = function(){
+  var csrf_token = $('meta[name=csrf-token]').attr('content');
+  var csrf_param = $('meta[name=csrf-param]').attr('content');
+
+  var params = {};
+  if (csrf_param !== undefined && csrf_token !== undefined) {
+      params[csrf_param] = csrf_token;
+  }
+  $('.redactor').redactor(
+    { "imageUpload":"/redactor_rails/pictures",
+      "uploadImageFields": params,
+      "imageGetJson":"/redactor_rails/pictures",
+      "path":"/assets/redactor-rails",
+      "css":"style.css"}
+  );
+}
+
+$(document).on( 'ready page:load', window.init_redactor );
